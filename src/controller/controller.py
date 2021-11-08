91afed36eab09d98a1f9520808a9621d9a3cd500
Shivanshu10 <smishra_be20@thapar.edu>
2021-11-08 14:09:34 +0530

    init commit

---
+++ src/controller/controller.py
@@ -0,0 +1,13 @@
+import pyautogui
+
+class Controller:
+    def __init__(self):
+        pass
+    def takeSS(self):
+        pass
+    def pressKey(self):
+        pass
+    def openDino(self):
+        pass
+    def closeDino():
+        pass
